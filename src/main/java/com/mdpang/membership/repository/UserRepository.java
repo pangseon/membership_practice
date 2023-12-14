c0a8161ae2382118a675ed003c5701c8fbc6734c
pangseon <chaosen0521@gmail.com>
2023-12-14 19:29:31 +0900

    회원가입 구현

---
+++ src/main/java/com/mdpang/membership/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mdpang.membership.repository;
+
+import com.mdpang.membership.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
